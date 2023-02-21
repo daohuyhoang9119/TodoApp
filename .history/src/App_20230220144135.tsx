a81583f8f0cee0b68348c9182d1bc8c80d28c4d4
HoangDH <daohuyhoang9119@gmail.com>
2023-02-21 22:47:32 +0700

    Update task item

--- .history/src/App_20230220144135.tsx
+++
@@ -1,9 +0,0 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import "./App.css";
-
-function App() {
-  return <h1 className=" text-slate-500 font-bold underline">Hello world!</h1>;
-}
-
-export default App;
